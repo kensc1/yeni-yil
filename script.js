b3f69f0aabff5db136d0c89d4dd9a3e25a5d929a
kensc1 <168110138+kensc1@users.noreply.github.com>
2025-12-31 23:14:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -82,8 +82,10 @@ document.addEventListener("DOMContentLoaded", () => {
           snowman.growing = false;
           snowman.exploded = true;
           createParticles();
+          // "Seni Seviyorum" sabit kalacak
           loveText.style.opacity = 1;
           loveText.style.transform = "translate(-50%, -50%) scale(1)";
+          loveText.style.position = "fixed";
         }
       }
 
